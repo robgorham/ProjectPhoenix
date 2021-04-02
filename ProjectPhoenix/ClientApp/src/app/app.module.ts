dffd284bd18e6685762634eb6176b86cf197f608
Gorham, Robert <gorhamr@upmc.edu>
2021-04-02 11:59:56 -0400

    Added the Board Module to UI

--- ProjectPhoenix/ClientApp/src/app/app.module.ts
+++ ProjectPhoenix/ClientApp/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { BoardModule } from './board/board.module';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
+    BoardModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
