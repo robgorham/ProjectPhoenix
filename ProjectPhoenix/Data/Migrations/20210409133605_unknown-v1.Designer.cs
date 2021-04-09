081a15d77a3c234c573b7d50a5a7dbc2fa4229c1
Gorham, Robert <gorhamr@upmc.edu>
2021-04-09 09:41:13 -0400

    feat: Updated .net to 5.0

--- ProjectPhoenix/Data/Migrations/20210409133605_unknown-v1.Designer.cs
+++ ProjectPhoenix/Data/Migrations/20210409133605_unknown-v1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectPhoenix.Data;
 
 namespace ProjectPhoenix.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210409133605_unknown-v1")]
+    partial class unknownv1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
