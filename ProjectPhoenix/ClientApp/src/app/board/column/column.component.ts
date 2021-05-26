5a5eb2ccfc6bb5dda7f36f1b2d06afbcf7e263ea
Gorham, Robert <gorhamr@upmc.edu>
2021-05-26 14:25:18 -0400

    initial breakout of column component

--- ProjectPhoenix/ClientApp/src/app/board/column/column.component.ts
+++ ProjectPhoenix/ClientApp/src/app/board/column/column.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-column',
@@ -9,6 +9,7 @@ export class ColumnComponent implements OnInit {
 
   constructor() { }
 
+  @Input() column: any;
   ngOnInit(): void {
   }
 
