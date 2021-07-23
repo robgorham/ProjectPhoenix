b3cb0ead9743e0a400e708e802953d2934f08bf5
Gorham, Robert <gorhamr@upmc.edu>
2021-07-23 13:53:17 -0400

    Creation of Model

---
+++ ProjectPhoenix/Models/IItemCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectPhoenix.Models
+{
+    interface IItemCard: IBaseModel
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        int Order { get; set; }
+        Board Board { get; set; }
+        Column Column { get; set; }
+        ApplicationUser User { get; set; }
+
+    }
+}
---
+++ ProjectPhoenix/Models/ItemCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectPhoenix.Models
+{
+    public class ItemCard :BaseModel, IItemCard
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Order { get; set; }
+        public virtual Board Board{ get; set; }
+        public virtual Column Column { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
