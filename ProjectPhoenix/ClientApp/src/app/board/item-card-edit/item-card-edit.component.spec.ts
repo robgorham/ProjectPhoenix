ec9409f7dac88b103a9a27dfa497f74ab5ca2a19
Gorham, Robert <gorhamr@upmc.edu>
2021-09-30 15:23:39 -0400

    fixed item-card edit

--- ProjectPhoenix/ClientApp/src/app/board/item-card-edit/item-card-edit.component.spec.ts
+++ ProjectPhoenix/ClientApp/src/app/board/item-card-edit/item-card-edit.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ItemCardEditComponent } from './item-card-edit.component';
 
@@ -8,9 +12,17 @@ describe('ItemCardEditComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ItemCardEditComponent ]
+      declarations: [ItemCardEditComponent],
+      imports: [MatDialogModule, MatFormFieldModule,
+        MatInputModule, NoopAnimationsModule],
+      providers: [{
+        provide: MAT_DIALOG_DATA, useValue: {
+          data: { name: 'Rob is cool', id: 1 }
+        }
+      },
+      { provide: MatDialogRef, useValue: {} },]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
