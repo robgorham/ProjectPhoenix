f341ec60585d9171b178a50f1e1bdb64dfd8c988
Gorham, Robert <gorhamr@upmc.edu>
2021-09-30 15:34:57 -0400

    column spec passing

--- ProjectPhoenix/ClientApp/src/app/board/column/column.component.spec.ts
+++ ProjectPhoenix/ClientApp/src/app/board/column/column.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { BoardApiService } from '../board-api.service';
 
 import { ColumnComponent } from './column.component';
 
@@ -8,7 +12,14 @@ describe('ColumnComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ColumnComponent ]
+      imports: [MatDialogModule, HttpClientTestingModule],
+      declarations: [ColumnComponent],
+      providers: [
+        {provide: 'BASE_URL', useValue: 'http://localhost'},
+        BoardApiService,
+        
+        { provide: MatDialogRef, useValue: {} },
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +27,13 @@ describe('ColumnComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ColumnComponent);
     component = fixture.componentInstance;
+    const col = { name: '1', itemCards: [] };
+    component.column = col;
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
